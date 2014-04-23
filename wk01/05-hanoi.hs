938fb0edf49c471c768d870e9e4f3c6a72a5734a
Prakash Kailasa <pk-github@kailasa.net>
2014-04-23 17:30:18 -0400

    remove unnecessary equation

--- wk01/05-hanoi.hs
+++ wk01/05-hanoi.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wall #-}
+
 -- CIS 194 -- Week 01
 
 -- Exercise 05
@@ -8,7 +10,7 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a b _ = [(a, b)]
-hanoi n a b c = (hanoi (n-1) a c b) ++ [(a, b)] ++ (hanoi (n-1) c b a)
+hanoi n a b c = (hanoi (n-1) a c b) ++ (hanoi 1 a b c) ++ (hanoi (n-1) c b a)
 
 -- Exercise 06
 -- use four pegs instead of three
@@ -17,5 +19,4 @@ hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 0 _ _ _ _ = []
 hanoi4 1 a b _ _ = [(a, b)]
 hanoi4 2 a b c _ = [(a, c), (a, b), (c, b)]
-hanoi4 3 a b c d = [(a, c), (a, d), (a, b), (d, b), (c, b)]
-hanoi4 n a b c d = (hanoi4 (n-2) a c b d) ++ (hanoi4 2 a b c d) ++ (hanoi4 (n-2) c b a d)
+hanoi4 n a b c d = (hanoi4 (n-2) a c d b) ++ (hanoi4 2 a b d c) ++ (hanoi4 (n-2) c b a d)
