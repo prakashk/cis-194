cfce27c7166051c947b496bf4c61fddc3933197a
Prakash Kailasa <pk-github@kailasa.net>
2014-05-03 19:27:30 -0400

    week 2 solutions

--- wk02/LogAnalysis.hs
+++ wk02/LogAnalysis.hs
@@ -7,6 +7,8 @@ module LogAnalysis where
 import Log
 import Data.Char (isSpace)
 
+-- Exercise 01
+
 parseRest :: String -> (Int, String)
 parseRest str =
   let [(t, rest)] = lex str
@@ -27,3 +29,33 @@ parseMessage message =
 
 parse :: String -> [LogMessage]
 parse = (map parseMessage) . lines
+
+-- Exercise 02
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mt = mt
+insert msg (Leaf) = Node Leaf msg Leaf
+insert msg@(LogMessage _ ts _) (Node left root@(LogMessage _ rts _) right) =
+  if ts < rts then
+     Node (insert msg left) root right
+  else
+    Node left root (insert msg right)
+
+-- Exercise 03
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+-- Exercise 04
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
+
+-- Exercise 05
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map getError . inOrder . build . filter isSevereError
+  where isSevereError (LogMessage (Error s) _ _) = s >= 50
+        isSevereError _ = False
+        getError (LogMessage (Error _) _ msg) = msg
