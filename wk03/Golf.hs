7a32be58f753f40b8f4f1c9dfdf65e30ea312ef9
Prakash Kailasa <pk-github@kailasa.net>
2014-05-05 17:54:34 -0400

    exercise 2 + golfed exercise 1

--- wk03/Golf.hs
+++ wk03/Golf.hs
@@ -3,7 +3,22 @@ module Golf where
 -- Exercise 01
 -- Hopscotch
 
-skips :: [a] -> [[a]]
-skips xs = map (\n -> nth n xs) [1..l]
-  where l = length xs
-        nth n xs = map snd $ filter (\(i, x) -> i `rem` n == 0) $ zip [1..l] xs
+-- skips :: [a] -> [[a]]
+-- skips xs = map (\n -> nth n xs) [1..l]
+--   where l = length xs
+--         nth n xs = map snd $ filter (\(i, x) -> i `rem` n == 0) $ zip [1..l] xs
+
+      -- nth n = (map snd) . (filter (\(i, x) -> i `rem` n == 0)) . (zip ns)
+      -- nth n xs = map snd $ filter (\(i, x) -> i `rem` n == 0) $ zip ns xs
+
+skips xs =
+  let ns = [1..length xs]
+      nth n = (map snd) . (filter (\x -> fst x `rem` n == 0)) . (zip ns)
+  in map (\n -> nth n xs) ns
+
+
+-- Exercise 02
+-- Local maxima
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = map (\(_, x, _) -> x) $ filter (\(a, b, c) -> b > a && b > c) $ zip3 xs (tail xs) (drop 2 xs)
