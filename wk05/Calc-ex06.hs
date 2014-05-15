e8628e1e639bd8115aa733bd83b71a5e0a993bc0
Prakash Kailasa <pk-github@kailasa.net>
2014-05-15 18:03:54 -0400

    Week 5, exercise 6 -- almost there?

---
+++ wk05/Calc-ex06.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Calc where
+
+import qualified Data.Map as M
+import Expr
+import Parser
+
+data VarExprT = Lit Integer
+              | Add VarExprT VarExprT
+              | Mul VarExprT VarExprT
+              | Var String
+                deriving (Eq, Show)
+
+type EvalVarMap = (M.Map String Integer -> Maybe Integer)
+
+
+instance Expr VarExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+class HasVars a where
+  var :: String -> a
+
+instance HasVars VarExprT where
+  var = Var
+
+-- instance HasVars (M.Map String Integer -> Maybe Integer) where
+instance HasVars EvalVarMap where
+  var = M.lookup
+
+myLift :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
+myLift f Nothing _ = Nothing
+myLift f _ Nothing = Nothing
+myLift f (Just a) (Just b) = Just (f a b)
+
+instance Expr EvalVarMap where
+  lit x = (\_ -> Just x)
+  add = undefined
+  mul = undefined
+  -- add x y = (\m -> Just (x $ m + y $ m))
+  -- mul x y = (\_ -> Just (x * y))
+
+withVars :: [(String, Integer)]
+            -> EvalVarMap
+            -> Maybe Integer
+withVars vs exp = exp $ M.fromList vs
